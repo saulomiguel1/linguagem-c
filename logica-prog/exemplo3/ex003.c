5ec19de338f99323ccc12ef54a6ec6ab16a88269
saulomiguel1 <saulovailma@gmail.com>
2026-02-11 15:23:56 -0300

    Saída de dados

---
+++ logica-prog/exemplo3/ex003.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    printf("Oi, tudo bem? Tenho 20 anos e programo em linguagem C!\n");
+
+    printf("Valor inteiro: %d. \n", 10);
+    printf("Valor real: %f. \n", 3.14159265);
+    printf("valor real com apenas 2 casas: %.2f.\n", 3.14159265);
+    printf("Dado de texto: %c.\n", 'a');
+    printf("Dado de texto: %s.\n", "testando");
+}
